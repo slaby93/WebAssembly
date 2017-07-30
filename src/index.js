f7925d72efc6e7be603e0134f84067f422bfa1b2
Daniel Słaby <pufek93@gmail.com>
2017-07-30 15:07:19 +0200

    Added load wasm file function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-fetch("./src/wasm/sqrt.wasm")
-  .then(resp => resp.arrayBuffer())
-  .then(bytes => WebAssembly.compile(bytes))
-  .then(module => WebAssembly.instantiate(module))
-  .then(instance => {
-    window.wasmSqrt = instance.exports.sqrt;
-  });
+const loadWasmFile = async (src, functionName, name) => {
+  await fetch(src)
+    .then(resp => {
+      return resp.arrayBuffer();
+    })
+    .then(bytes => WebAssembly.compile(bytes))
+    .then(module =>  WebAssembly.instantiate(module))
+    .then(instance => {
+      window[name] = instance.exports[functionName];
+    });
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+  await loadWasmFile("./src/wasm/sqrt.wasm", "sqrt", "wasmSqrt");
+  await loadWasmFile("./src/wasm/program.wasm", "test", "wasmCalc");
+  console.log(window.wasmCalc(5.99,59.99));
+});
