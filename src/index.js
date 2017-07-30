b93208b50c4ea640182ca65685007af8da29b7f2
Daniel Słaby <pufek93@gmail.com>
2017-07-30 14:04:17 +0200

    First load of web assembly function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-const a = (x) => x**4;
-
-console.log(a(2));
+fetch("./src/wasm/sqrt.wasm")
+  .then(resp => resp.arrayBuffer())
+  .then(bytes => WebAssembly.compile(bytes))
+  .then(module => WebAssembly.instantiate(module))
+  .then(instance => {
+    window.wasmSqrt = instance.exports.sqrt;
+  });
